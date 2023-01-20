ec2111746148449e453d0e401928ba68819cf79f
Daniel Tanase <tanasedaniel54@gmail.com>
2023-01-21 00:43:14 +0200

    feat: next auth with email provider

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,26 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { NextApiHandler } from 'next';
+import NextAuth from 'next-auth';
+import EmailProvider from 'next-auth/providers/email';
+import prisma from '../../../lib/prisma';
+
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
+
+const options = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+};
+
+export default authHandler;
