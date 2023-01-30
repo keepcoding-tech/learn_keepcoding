e94442cca00bb857ba0fa073ca09d303cd28de8c
Daniel Tanase <tanasedaniel54@gmail.com>
2023-01-30 21:28:26 +0200

    feat: redesigne the page template

--- components/templates/page/PageTemplate.tsx
+++ components/templates/page/PageTemplate.tsx
@@ -1,5 +1,15 @@
 import { Edit } from '@mui/icons-material';
-import { Paper, Typography } from '@mui/material';
+import BookmarkAddIcon from '@mui/icons-material/BookmarkAdd';
+import ThumbDownIcon from '@mui/icons-material/ThumbDown';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import {
+  Box,
+  Button,
+  Grid,
+  IconButton,
+  Paper,
+  Typography,
+} from '@mui/material';
 import Router from 'next/router';
 import React from 'react';
 import MarkdownPreview from '../../markdown/MarkdownPreview';
@@ -25,6 +35,7 @@ const PageTemplate: React.FC<IPageTemplate> = (page) => {
       sx={{
         bgcolor: 'var(--cards-color)',
         color: 'var(--secondary-font-color)',
+        padding: 1,
       }}
     >
       <Edit
@@ -32,12 +43,60 @@ const PageTemplate: React.FC<IPageTemplate> = (page) => {
           Router.push('/docs/edit?id=' + page.id);
         }}
       />
-      <Typography variant="h3">{page.title}</Typography>
+      <Grid container rowSpacing={2}>
+        <Grid item xs={6}>
+          <Typography sx={{ marginLeft: 5 }}>
+            Module &gt; Chapter &gt; Doc
+          </Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <Box sx={{ float: 'right', marginRight: 5 }}>
+            Was this helpful?
+            <>&nbsp;&nbsp;</>
+            <IconButton>
+              <ThumbUpIcon
+                fontSize="small"
+                style={{ color: 'var(--secondary-font-color)' }}
+              />
+            </IconButton>
+            <>&nbsp;&nbsp;</>
+            <IconButton>
+              <ThumbDownIcon
+                fontSize="small"
+                style={{ color: 'var(--secondary-font-color)' }}
+              />
+            </IconButton>
+          </Box>
+        </Grid>
+        <Grid item xs={8} sx={{ marginLeft: 5 }}>
+          <h2>{page.title}</h2>
+        </Grid>
+        <Grid item xs={3}>
+          <Box sx={{ float: 'right' }}>
+            <IconButton>
+              <BookmarkAddIcon
+                style={{ color: 'var(--secondary-font-color)' }}
+              />
+            </IconButton>
+            <>&nbsp;&nbsp;</>
+            <Button
+              variant="outlined"
+              sx={{
+                color: 'var(--secondary-font-color)',
+                borderColor: 'var(--secondary-font-color)',
+              }}
+            >
+              Send feedback
+            </Button>
+          </Box>
+        </Grid>
+        <Grid item xs={12}>
+          <MarkdownPreview>{page.content}</MarkdownPreview>
+        </Grid>
+      </Grid>
       <Typography variant="body2">
         by {page.author?.name || page.author?.email}
       </Typography>
-      <br />
-      <MarkdownPreview>{page.content}</MarkdownPreview>
     </Paper>
   );
 };
